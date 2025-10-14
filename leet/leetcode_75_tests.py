835510f2d0657e9b6e689f655a31f166fab4f500
Helio Neto <helio.hetfield@gmai.com>
2025-10-14 07:51:51 -0300

    feat: add initial problem definition

--- leet/leetcode_75_tests.py
+++ leet/leetcode_75_tests.py
@@ -492,7 +492,81 @@ class Solutions:
             if broken_letter in text:
                 can_be_typed_words -= text.count(broken_letter)
         return can_be_typed_words
+    
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        You are given two non-empty linked lists representing two non-negative integers. The digits are
+        stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and
+        return the sum as a linked list.
 
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+        Example 1:
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+
+        Example 2:
+        Input: l1 = [0], l2 = [0]
+        Output: [0]
+
+        Example 3:
+        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        Output: [8,9,9,9,0,0,0,1]
+
+        Constraints:
+        The number of nodes in each linked list is in the range [1, 100].
+        0 <= Node.val <= 9
+        It is guaranteed that the list represents a number that does not have leading zeros.
+
+        ref: https://leetcode.com/problems/add-two-numbers/
+        """
+    
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        """
+        You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+        In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i]
+        are anagrams, and delete words[i] from words. Keep performing this operation as long as you can
+        select an index that satisfies the conditions.
+
+        Return words after performing all operations. It can be shown that selecting the indices for each
+        operation in any arbitrary order will lead to the same result.
+
+        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase
+        using all the original letters exactly once. For example, "dacb" is an anagram of "abdc".
+
+        Example 1:
+
+        Input: words = ["abba","baba","bbaa","cd","cd"]
+        Output: ["abba","cd"]
+        Explanation:
+        One of the ways we can obtain the resultant array is by using the following operations:
+            - Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index 2 and delete words[2].
+        Now words = ["abba","baba","cd","cd"].
+            - Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index 1 and delete words[1].
+        Now words = ["abba","cd","cd"].
+            - Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2 and delete words[2].
+        Now words = ["abba","cd"].
+        We can no longer perform any operations, so ["abba","cd"] is the final answer.
+        
+        Example 2:
+
+        Input: words = ["a","b","c","d","e"]
+        Output: ["a","b","c","d","e"]
+        Explanation:
+            - No two adjacent strings in words are anagrams of each other, so no operations are performed.
+ 
+
+        Constraints:
+
+        1 <= words.length <= 100
+        1 <= words[i].length <= 10
+        words[i] consists of lowercase English letters.
+
+        ref: https://leetcode.com/problems/remove-anagrams/
+        """
+
+        
 
 class RecentCounter:
 
