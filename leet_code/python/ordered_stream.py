3daf21dd5b21b5c3fb7852e3a8892e1c8a6a77ca
Hélio Corrêa da Silva Neto <helio.hetfield@gmail.com>
2026-07-25 20:01:18 -0300

    feat: add python problem and documentation adjustments (#15)
    Co-authored-by: Helio Neto <helio.hetfield@gmai.com>

--- leet_code/python/ordered_stream.py
+++ leet_code/python/ordered_stream.py
@@ -1,5 +1,17 @@
 class OrderedStream:
+    """
+    OrderedStream is a data structure that receives a stream of (id, value) pairs 
+    and returns values in increasing order of their IDs by returning chunks (lists) of values 
+    after each insertion.
+    
+    The concatenation of all the chunks should result in a list of the sorted values.
 
+    Example:
+    Your OrderedStream object will be instantiated and called as such:
+    obj = OrderedStream(n)
+    param_1 = obj.insert(idKey,value)
+
+    """
     def __init__(self, n: int):
         self.n = n
         self.stream = [None] * n
@@ -12,8 +24,3 @@ class OrderedStream:
             result.append(self.stream[self.ptr])
             self.ptr += 1
         return result
-
-
-# Your OrderedStream object will be instantiated and called as such:
-# obj = OrderedStream(n)
-# param_1 = obj.insert(idKey,value)
--- leet_code/python/recent_counter.py
+++ leet_code/python/recent_counter.py
@@ -1,13 +1,15 @@
-
 class RecentCounter:
     """ 
     Status: COMPLETE
-    Ref: https://leetcode.com/problems/number-of-recent-calls/
+
+    Reference: https://leetcode.com/problems/number-of-recent-calls/
+
     Description:
     You have a RecentCounter class which counts the number of recent requests within a certain time frame.
 
     int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns
     the number of requests that has happened in the past 3000 milliseconds (including the new request).
+
     Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
     It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
     """
@@ -16,8 +18,15 @@ class RecentCounter:
         self.recent_requests = []
 
     def ping(self, time_in_milliseconds: int) -> int:
-        """ Adds a new request representing the time in milliseconds and returns the number of requests that
+        """
+        Adds a new request representing the time in milliseconds and returns the number of requests that
         has happened in the past 3000 milliseconds (including the new request).
         """
         self.recent_requests.append(time_in_milliseconds)
-        return len([request for request in self.recent_requests if request >= time_in_milliseconds - 3000 and request <= time_in_milliseconds])
+        return len(
+            [
+                request 
+                for request in self.recent_requests 
+                if request >= time_in_milliseconds - 3000 and request <= time_in_milliseconds
+            ]
+        )
